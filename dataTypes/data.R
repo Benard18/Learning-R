066f4af6024f08b8a35245dbddddd2b89e966a1b
Ben Karanja <benkaranja43@gmail.com>
2018-11-26 11:30:41 +0300

    Learning on making data types in R

---
+++ dataTypes/data.R
@@ -0,0 +1,28 @@
+# Logical
+v <- TRUE
+print(class(v))
+print(v)
+
+#Numeric
+vv <- 23.5
+print(class(vv))
+print(vv)
+
+#Integer
+zz <- 2L
+print(class(zz))
+print(zz)
+
+#Complex
+xy <- 2+5i
+print(class(xy))
+print(xy)
+
+#Character
+zx <- "TRUE"
+print(class(zx))
+print(zx)
+#RAW
+zxx <- charToRaw("Hello")
+print(class(zxx))
+print(zxx)
